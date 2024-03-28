d64d5771757e450299bb35e17e633c8c8c669557
Zeeshankhan89 <muhammadzeeshan2353@gmail.com>
2024-03-28 11:12:11 +0500

    28a

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http')
-const dat = require('./data')
+const date = require('./data')
 http.createServer((req, res)=>{
     res.writeHead(200,{'Content-Type':'application/json'});
-    res.write(JSON.stringify(dat));
+    res.write(JSON.stringify(date));
     res.end();
 }).listen(5000);
 console.log("new changes")
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const fs = require('fs')
 const path = require('path')
+const dirPath = path.join(__dirname, 'crud')
+const filePath = `${dirPath}/fruit.txt`
 
-const dirPath = path.join(__dirname, 'files')
-// fs.writeFileSync(dirPath+'/khan.txt', 'hello zeeshan how are you')
-// for(i=0; i<5; i++){
-//     fs.writeFileSync(dirPath+"/hello"+i+".txt", "hello")
-// }
-fs.readdir(dirPath, (err, files)=>{
-    files.forEach((item)=>{
-    console.log(item)
-})
-})
+// fs.writeFileSync(filePath, 'hi, it is fruit') //for create a file
+// fs.readFile(filePath, 'utf8',(err, item)=>{ // for read a file
+//     console.log(item)
+// })
+
+// fs.appendFile(filePath,' and file name is app.txt',(err)=>{  // for update the content of file
+//     if(!err) console.log('file is updated')
+// })
+//  fs.rename(filePath,`${dirPath}/fruit2.txt`, (err)=>{ //for rename the file name
+//     if(!err) console.log('File name is updated')
+//  })
+fs.unlinkSync(`${dirPath}/fruit2.txt`)
